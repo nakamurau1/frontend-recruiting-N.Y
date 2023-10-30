c316eb46fc3304360f933529032d12f16116ec07
nakamurau1 <fyuichi@gmail.com>
2023-10-30 21:34:53 +0900

    charge関数をリファクタリング
    - calculateDepositとisAllPaymentsCouponを独立したユーティリティ関数として抽出
    - calculateDeposit内のソートロジックをより明確に更新

--- assignment-1/src/charge.ts
+++ assignment-1/src/charge.ts
@@ -9,42 +9,57 @@ export type Receipt = {
 };
 
 export type Payment = {
-  type: string;
+  type: 'CASH' | 'COUPON';
   percentage?: number;
   amount?: number;
 };
 
-export function charge(invoice: Invoice, payments: Payment[]) {
-  const total = invoice.total;
+// 支払いの合計を計算
+function calculateDeposit(invoiceTotal: number, payments: Payment[]): number {
   let deposit = 0;
 
-  payments
-    .sort((payment) => (payment.type !== 'CASH' ? -1 : 1))
-    .map((payment) => {
-      if (payment.type === 'COUPON') {
-        if (payment.percentage != null) {
-          deposit += Math.floor(total * (payment.percentage / 100));
-        } else {
-          deposit += payment.amount || 0;
-        }
+  // 支払いをソート: 現金(CASH)の支払いを後ろに移動させる
+  payments.sort((a, b) => {
+    if (a.type === 'CASH' && b.type !== 'CASH') {
+      return 1;
+    }
+    if (a.type !== 'CASH' && b.type === 'CASH') {
+      return -1;
+    }
+    return 0;
+  });
+
+  for (const payment of payments) {
+    if (payment.type === 'COUPON') {
+      if (payment.percentage != null) {
+        deposit += Math.floor(invoiceTotal * (payment.percentage / 100));
       } else {
-        if (deposit >= total) {
-          throw new Error('OverCharge');
-        }
         deposit += payment.amount || 0;
       }
-    });
+    } else {
+      if (deposit >= invoiceTotal) {
+        throw new Error('OverCharge');
+      }
+      deposit += payment.amount || 0;
+    }
+  }
+  return deposit;
+}
+
+// すべての支払いがクーポンかどうかをチェック
+function isAllPaymentsCoupon(payments: Payment[]): boolean {
+  return payments.every((payment) => payment.type === 'COUPON');
+}
+
+export function charge(invoice: Invoice, payments: Payment[]): Receipt {
+  const total = invoice.total;
+  const deposit = calculateDeposit(total, payments);
+
   if (total > deposit) {
     throw new Error('Shortage');
   }
 
-  let isCoupon = true;
-  for (let i = 0; i < payments.length; i++) {
-    if (payments[i].type !== 'COUPON') {
-      isCoupon = false;
-      continue;
-    }
-  }
-  if (isCoupon) return { total, deposit, change: 0 };
-  return { total: total, deposit: deposit, change: deposit - total };
+  const change = isAllPaymentsCoupon(payments) ? 0 : deposit - total;
+
+  return { total, deposit, change };
 }
